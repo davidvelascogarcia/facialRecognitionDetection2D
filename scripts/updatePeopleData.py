2c809e8e8c60f422063018174c679e51d1ddb5f5
davidvelascogarcia <davelasc@ing.uc3m.es>
2020-07-26 14:24:18 +0200

    Improve sripts format

--- scripts/updatePeopleData.py
+++ scripts/updatePeopleData.py
@@ -4,25 +4,26 @@
  *      Type: Python
  *      Author: David Velasco Garcia @davidvelascogarcia
  * ************************************************************
- */
 '''
 
 # Libraries
+import datetime
 import os
 import numpy as np
 import string
 import sys
 
 print("")
+print("**************************************************************************")
+print("Updating people data labels:")
+print("**************************************************************************")
+print("")
+print("[INFO] Preparing and updating people labels at " + str(datetime.datetime.now()) + " ...")
+print("")
+
 print("")
 print("**************************************************************************")
-print("updatePeopleData")
-print("**************************************************************************")
-print("")
-print("Preparing and updating people labels ...")
-print("")
-print("**************************************************************************")
-print("Processing:")
+print("Processing input people date labels:")
 print("**************************************************************************")
 print("")
 
@@ -31,12 +32,16 @@ loopControlFileExists = 0
 while int(loopControlFileExists)==0:
     try:
         print("")
-        print("Reading peopleFiles.txt ...")
+        print("[INFO] Reading peopleFiles.txt at " + str(datetime.datetime.now()) + " ...")
+        print("")
+
         peopleFile = open("../resources/peopleFiles.txt", "rt")
+
         loopControlFileExists = 1
+
     except:
         print("")
-        print("Sorry, peopleFiles.txt not founded, waiting 4 seconds to the next check ...")
+        print("[ERROR] Sorry, peopleFiles.txt not founded, waiting 4 seconds to the next check ...")
         print("")
         time.sleep(4)
 
@@ -44,17 +49,20 @@ peopleFileUpdated = open("../resources/peopleData.txt", "wt")
 
 # Clean image extension and to uppercase
 print("")
-print("Writing people labels ...")
+print("[INFO] Writing people labels at " + str(datetime.datetime.now()) + " ...")
+print("")
+
 for line in peopleFile:
-	cleanNames=line.replace('.jpg', '')
-	cleanNames=cleanNames.replace('.jpeg', '')
-	cleanNames=cleanNames.replace('.giff', '')
-	cleanNames=cleanNames.replace('.tiff', '')
-	cleanNames=cleanNames.replace('.png', '')
-	cleanNames=cleanNames.replace('.bmp', '')
-	cleanNames=cleanNames.replace('.pdf', '')
-	cleanNames=cleanNames.replace('database/', '')
-	cleanNames=cleanNames.upper()
+
+	cleanNames = line.replace('.jpg', '')
+	cleanNames = cleanNames.replace('.jpeg', '')
+	cleanNames = cleanNames.replace('.giff', '')
+	cleanNames = cleanNames.replace('.tiff', '')
+	cleanNames = cleanNames.replace('.png', '')
+	cleanNames = cleanNames.replace('.bmp', '')
+	cleanNames = cleanNames.replace('.pdf', '')
+	cleanNames = cleanNames.replace('database/', '')
+	cleanNames = cleanNames.upper()
 	peopleFileUpdated.write(cleanNames)
 
 # Close files
@@ -62,13 +70,16 @@ peopleFile.close()
 peopleFileUpdated.close()
 
 print("")
-print("People labels updated correctly")
+print("[INFO] People labels updated correctlyat " + str(datetime.datetime.now()) + ".")
+print("")
 
 print("")
 print("")
 print("**************************************************************************")
 print("Program finished")
 print("**************************************************************************")
-print()
-print("Press enter to close program ...")
+print("")
+print("[INFO] Press enter to close program ...")
+print("")
+
 exitProgram = input()
--- scripts/updatePeopleFiles.sh
+++ scripts/updatePeopleFiles.sh
@@ -1,10 +1,9 @@
 echo "**********************************************************"
-echo "updatePeopleFiles"
+echo "Updating people files database:"
 echo "**********************************************************"
 echo ""
-echo "Updating people files database ..."
+echo "[INFO] Updating people files database ..."
 echo ""
-
 
 cd ..
 mkdir resources
@@ -12,16 +11,25 @@ cd database
 
 echo ""
 echo "**********************************************************"
-echo "Procesing:"
+echo "Procesing input people files database:"
 echo "**********************************************************"
+echo ""
+echo "[INFO] Procesing input people files database ..."
 echo ""
 
 ls > ../resources/peopleFiles.txt
 
 echo ""
-echo "People files database configured correctly."
-echo "Now execute updatePeopleDate.py to prepare people labels"
+echo "[INFO] People files database configured correctly."
+echo ""
+
+echo ""
+echo "[INFO] Now execute updatePeopleDate.py to prepare people labels."
 echo ""
 echo "**********************************************************"
 echo "Program finished"
 echo "**********************************************************"
+echo ""
+echo "updatePeopleFiles program finished correctly."
+echo ""
+
